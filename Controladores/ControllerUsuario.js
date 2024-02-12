7f360d21947b0979bc0104404dfa98c379ce1c38
AntoGamboa <antoniojose2814@hotmail.com>
2024-02-12 13:30:47 -0400

    login

--- Controladores/ControllerUsuario.js
+++ Controladores/ControllerUsuario.js
@@ -1,8 +1,9 @@
 let Enviarformulario= document.getElementById('formregistro');
+let formLogin= document.getElementById('formLogin');
+
 Enviarformulario.addEventListener('submit',function(e)
 {
     e.preventDefault();
-    console.log("me diste click")
     let formData= new FormData(Enviarformulario);
     formData.append("accion","crearregistro");
     fetch('https://localhost/Proyecto_ClinicasV2/Modelos/modelUsuarios.php',
@@ -12,18 +13,26 @@ Enviarformulario.addEventListener('submit',function(e)
     })
     .then(resp => 
     {
-        if(resp.ok)
-        {
-            console.log("comunicacion Exitosa");
-        }
        return resp.json()
-
     })
-    .then(data => 
-        {
+    .then(data =>{
             console.log(data);
-        })
+    })
     .catch(error => console.error("error: ", error.message));
-   
+});
+
+formLogin.addEventListener('submit', e => {
+    e.preventDefault();
+    let formdata = new FormData(formLogin);
+    formdata.append("accion","logearse");
+    fetch( 'https://localhost/Proyecto_ClinicasV2/Modelos/modelUsuarios.php', 
+    {
+        method:'POST',
+        body:formdata 
+    })
+    .then( resp =>resp.json())
+    .then(data => {
+        console.log(data);
+    });
 });
 
--- Modelos/modelUsuarios.php
+++ Modelos/modelUsuarios.php
@@ -37,28 +37,47 @@
             $this->getConexion()->prepare($query)->execute(array($nombre, $apellido, $correo, $usuario, $password));
 
         }
+        public function logearse($correo,$password)
+        {
+            $query = <<<eot
+                Select * from usuarios where correo = ? and contraseña = ?
+            eot;
+            $stmt=$this->getConexion()->prepare($query);
+            $stmt->execute(array($correo,$password));
+            return json_encode($stmt->fetch(PDO::FETCH_OBJ));
+        
+        }
     }
-
     $objUsuario=new Usuarios();
     $accion=$_POST["accion"];
-    $nombre = $_POST["nombre"];
-    $apellido = $_POST["apellido"];
-    $correo = $_POST["correo"];
-    $usuario = $_POST["usuario"];
-    $password = $_POST["Contraseña"];
     if(isset($accion) )
     {
         try
         {
-            $objUsuario->$accion($nombre , $apellido ,$correo, $usuario, $password); 
-            echo json_encode(["mensaje" => "Registro exitoso"]);
+            if($accion === 'crearregistro'){
+                
+                $nombre = $_POST["nombre"];
+                $apellido = $_POST["apellido"];
+                $correo = $_POST["correo"];
+                $usuario = $_POST["usuario"];
+                $password = $_POST["Contraseña"];
+                if(empty($nombre)||empty($apellido)||empty($correo)||empty($usuario)||empty($correo))
+                {
+                    echo json_encode(["mensaje" => "Existen campos vacios"]);
+                } else{
+                    $objUsuario->$accion($nombre , $apellido ,$correo, $usuario, $password); 
+                    echo json_encode(["mensaje" => "Registro exitoso"]);
+                }     
+            }
+            if($accion === 'logearse'){
+                $correo = $_POST["correo"];
+                $password = $_POST["Contraseña"];
+                echo $objUsuario->logearse($correo,$password);
+            }
         }
         catch(Exception $e)
         {
             echo json_encode(["mensaje" => $e->getMessage()]);
         }
     }
-
-
-
 ?>
