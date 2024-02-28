4b86270a88ab49c6ecc68ba3d6bfefc81fe2d66e
RCsharp45 <ricardorespaldo2304@gmail.com>
2024-02-28 19:50:20 -0400

    eventos de consulta form completados y ajustes en el css
    mas informacion en los comentarios

--- Controladores/ControllerConsulta.js
+++ Controladores/ControllerConsulta.js
@@ -1,40 +1,19 @@
-document.addEventListener('DOMContentLoaded',e =>{
-
-   
 
 
-    let formdata = new FormData();
-    formdata.append("accion","readAll");
-    fetch('../Modelos/.php',{
-        method:'POST',
-        body:formdata
-    })
-    .then(response => response.json())
-    .then(data => {
 
-        console.log(data);
 
-        const dataTableBody = document.getElementById('tabla_datos');
-        
-        data.forEach(medico => {
-            
-            alert("hola");
 
-            const newrow = document.createElement('tr');
 
-            newrow.innerHTML = `
-            <td>${medico.cedula}</td>
-            <td>${medico.nombre}</td>
-            <td>${medico.apellido}</td>
-            <td>
-                <button  type="button" class="edit-button" >Editar</button>
-                <button  type="button" class="delete-button">Eliminar</button>
-            </td>
-            `;
-
-            dataTableBody.appendChild(newrow);
-
-        });
-    })
+document.addEventListener('submit',e => {
     
+    if(funcion = "registrar"){
+
+        alert("hola");
+    }
+    else if(funcion = "modificar"){
+
+        alert("hola modifico");
+    }
+
+
 });
